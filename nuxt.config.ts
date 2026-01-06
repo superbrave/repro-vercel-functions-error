63dc5e18cf2229f54f68c8aa3984ff65c46c4d10
Jorik Rovers <jr@ehvg.nl>
2026-01-06 10:40:04 +0100

    Update caching headers in nuxt.config.ts for improved CDN performance and differentiate caching strategies for various routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,29 @@ export default defineNuxtConfig({
       isr: 300,
       headers: {
         "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Netlify-CDN-Cache-Control":
+          "no-cache, no-store, must-revalidate, max-age=0, stale-while-revalidate=0",
       },
     },
     "/**": {
+      isr: 300,
       headers: {
-        "Cache-Control": "public, s-maxage=300, stale-while-revalidate=604800",
+        // Cache for 5 minutes, stale for 7 days (while being revalidated/refreshed in the background)
+        // The durable directive is used to share the cache across all edge nodes.
+        // See: https://docs.netlify.com/platform/caching/#stale-while-revalidate-directive
+        "Netlify-CDN-Cache-Control":
+          "public, durable, max-age=300, stale-while-revalidate=604800",
+        // Differentiate caching for personalization and search queries
+        "Netlify-Vary":
+          "query=prepr_preview_ab|prepr_preview_segment|q,cookie=__prepr_uid",
 
+        // Vercel only
+        "Cache-Control": "public, s-maxage=300, stale-while-revalidate=604800",
         Vary: "Cookie",
       },
     },
     "/api/**": {
-      isr: true,
+      isr: 300,
       headers: {
         "Netlify-Vary": "query",
       },
