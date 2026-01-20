4c0f6c1fad10e926d0a869f589811bb94147c5cc
Jorik Rovers <jr@ehvg.nl>
2026-01-20 15:54:18 +0100

    Correct typos in index.vue for 'shopping cart' and 'account' proxy page links, enhancing clarity in navigation.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,12 +9,12 @@
 
     <br /><br />
     <NuxtLink external to="/nl-nl/redirect?name=cart">
-      Link to a shopping cartproxy page
+      Link to a shopping cart proxy page
     </NuxtLink>
 
     <br /><br />
     <NuxtLink external to="/nl-nl/redirect?name=account">
-      Link to a accountproxy page
+      Link to a account proxy page
     </NuxtLink>
   </div>
 </template>
